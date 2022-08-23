6ef83a93216c543f599dee7bac7452c912e7e4b4
Desha-rgb <mnsar747@gmail.com>
2022-08-23 21:51:13 +0200

    Initial Commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '402cc71d-6b28-4c3d-b6e9-8ca01289184e';
+export const MODAL_CLOSE_SEC = 2.5;
